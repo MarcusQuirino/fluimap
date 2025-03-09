fdb98215023d0ded64b808a635e374d48d7d923f
MarcusQuirino <marcusfilipus@gmail.com>
2025-03-09 19:11:05 -0300

    formating

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -5,20 +5,20 @@ import { Resend } from "resend";
 const resend = new Resend(env.RESEND_API_KEY);
 
 export async function POST() {
-    try {
-        const { data, error } = await resend.emails.send({
-            from: 'Acme <onboarding@resend.dev>',
-            to: ['delivered@resend.dev'],
-            subject: 'Your survey is waiting',
-            react: SurveyEmail({ username: "Joe Doe" }),
-        });
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: ["delivered@resend.dev"],
+      subject: "Your survey is waiting",
+      react: SurveyEmail({ username: "Joe Doe" }),
+    });
 
-        if (error) {
-            return Response.json({ error }, { status: 500 });
-        }
-
-        return Response.json(data);
-    } catch (error) {
-        return Response.json({ error }, { status: 500 });
+    if (error) {
+      return Response.json({ error }, { status: 500 });
     }
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
 }
